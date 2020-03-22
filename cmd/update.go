efc2ecda04d7e3463c067184c774068baa1306ef
kiddynamo <njenan@gmail.com>
2020-03-21 20:25:42 -0700

    Adding more cli commands

--- cmd/update.go
+++ cmd/update.go
@@ -1,18 +1,32 @@
 package cmd
 
 import (
+	"git.nathanjenan.me/njenan/godaddy-dns-updater/updater"
 	"github.com/spf13/cobra"
 )
 
+var endpoint string
+var recordNames []string
+var dryRun bool
+
 func init() {
+	updateCmd.Flags().StringVarP(&endpoint, "endpoint", "e", "", "API endpoint to use")
+	updateCmd.Flags().BoolVarP(&dryRun, "dry-run", "d", false, "Dry-run (report what will be updated)")
+	updateCmd.Flags().StringSliceVarP(&recordNames, "record-names", "r", []string{}, "List of A record names to update (if not specified, all found A records will be updated)")
+
 	rootCmd.AddCommand(updateCmd)
 }
 
 var updateCmd = &cobra.Command{
-	Use:   "update",
+	Use:   "update [DOMAIN] [IP]",
 	Short: "Check and update A records for the specified domain",
 	Long:  "Check and update A records for the specified domain to the specified IP address",
+	Args:  cobra.ExactArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
+		domain := args[0]
+		ip := args[1]
 
+		updater := updater.Updater{}
+		updater.CheckAndUpdate(domain, ip)
 	},
 }
--- cmd/version.go
+++ cmd/version.go
@@ -15,6 +15,7 @@ func init() {
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version number of GoDaddy DNS Updater",
+	Args:  cobra.NoArgs,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println(Version)
 	},
